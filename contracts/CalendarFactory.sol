108b1b9965edbc43b44d13a65ad82d08b64a6eff
Hunter King <hking6776@gmail.com>
2022-09-21 15:52:33 -0400

    add CloneFactory contract

--- contracts/CalendarFactory.sol
+++ contracts/CalendarFactory.sol
@@ -1,16 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import "./UserCalendar.sol";
+import "./CloneFactory.sol";
 
-contract CalendarFactory {
-  UserCalendar[] private _userCalendar;
+contract CalendarFactory is CloneFactory {
+  mapping(address => UserCalendar) userCalendars;
+  UserCalendar[] public userCalendarsArray;
 
   event UserCalCreated(address userCalAddress);
 
   function createUserCal(address communityTracker) external {
-    UserCalendar userCalendar = new UserCalendar(communityTracker);
-    _userCalendar.push(userCalendar);
+    UserCalendar userCalendar = UserCalendar(createClone(target));
+
+    userCalendars[msg.sender] = userCalendar;
+    UserCalendar.push(userCalendar);
 
     emit UserCalCreated(communityTracker);
+  }
+
+  function getUserCalendarClone(address user) external view returns (address userCalendar) {
+    return userCalendars[user];
+  }
+
+  function getAllUserCalendarClones(address user) external view returns (UserCalendar[] memory) {
+    return userCalendarsArray;
   }
 }
